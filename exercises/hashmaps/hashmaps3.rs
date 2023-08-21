fd067032ba64020f90b3502d1f1e1bcda676087e
neerajadsul <neeraj.adsul@gmail.com>
2023-08-21 18:29:03 +0100

    HashMap creation, entry() pattern to modify or insert.

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -16,9 +16,10 @@
 
 // I AM NOT DONE
 
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Add};
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -39,6 +40,13 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        scores.entry(team_1_name)
+        .and_modify(|e| {e.goals_conceded += team_2_score; e.goals_scored += team_1_score})
+        .or_insert(Team { goals_scored: team_1_score, goals_conceded: team_2_score });
+        
+        scores.entry(team_2_name)
+        .and_modify(|e| {e.goals_conceded += team_1_score; e.goals_scored += team_2_score})
+        .or_insert(Team { goals_scored: team_2_score, goals_conceded: team_1_score });
     }
     scores
 }
